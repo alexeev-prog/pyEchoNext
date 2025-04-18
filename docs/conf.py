1ed6c5f9a3d37a8000d76a49db5a24925e6d25e2
Alexeev Bronislav <alexeev.dev@mail.ru>
2025-04-19 05:16:08 +0700

    docs: fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,7 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 sys.path.insert(0, os.path.abspath('../pyechonext'))
+sys.path.insert(0, os.path.abspath('pyechonext'))
 
 project = "pyEchoNext"
 author = "name"
