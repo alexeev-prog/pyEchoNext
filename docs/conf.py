feb5219e63cd6330e88767f69d6e81e4fa12f802
Alexeev Bronislav <alexeev.dev@mail.ru>
2025-04-18 21:53:38 +0700

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -8,24 +8,18 @@ version = '0.7.14'
 release = '0.7'
 project_copyright = "2025, Alexeev Bronislaw"
 
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'restructuredtext',
-    '.md': 'markdown',
-}
-
 extensions = [
-    'sphinx.ext.autodoc',  # авто документации из docstrings
-    'sphinx.ext.viewcode',  # ссылки на исходный код
-    'sphinx.ext.napoleon',  # поддержка Google и NumPy стиля документации
-    'sphinx.ext.todo',  # поддержка TODO
-    'sphinx.ext.coverage',  # проверяет покрытие документации
-    'sphinx.ext.ifconfig',  # условные директивы в документации
+    'sphinx.ext.autodoc',    # autodoc from docstrings
+    'sphinx.ext.viewcode',    # links to source code
+    'sphinx.ext.napoleon',    # support google and numpy docs style
+    'sphinx.ext.todo',        # support TODO
+    'sphinx.ext.coverage',    # check docs coverage
+    'sphinx.ext.ifconfig',    # directives in docs
 ]
 
-html_theme = 'furo'  # тема оформления
-html_static_path = ['_static']  # папка со статическими файлами (например, CSS)
-todo_include_todos = True  # показывать TODO в готовой документации
+html_theme = 'furo'  # theme
+html_static_path = ['_static']  # static dir
+todo_include_todos = True  # include todo in docs
 auto_doc_default_options = {'autosummary': True}
 
-autodoc_mock_imports = []  # модули для мокирования
+autodoc_mock_imports = []
